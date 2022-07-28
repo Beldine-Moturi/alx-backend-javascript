e96525006a8ae29ca1da21b5cb4864aa4c83d3c0
Beldine-Moturi <beldinemoturi@gmail.com>
2022-07-29 00:47:16 +0300

    Task 0

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'linebreak-style': 0,
+  },
+};
