afbcb666f764b454c1ce75a23718119ac1525033
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-02 03:47:01 +0300

    task 2: getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Not a string');
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw new TypeError('Not a number');
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (typeof students !== 'object') throw new TypeError('Not an object');
+    this._students = students;
+  }
+}
