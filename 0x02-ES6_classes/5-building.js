2dcb13a577b52739827f9ed3874e0570b19c95cf
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-02 22:45:38 +0300

    fix errors

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,14 @@
 export default class Building {
-  constructor() {
-    if (this.evacuationWarningMessage === undefined) throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    if (
+      new.target !== Building && this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 }
