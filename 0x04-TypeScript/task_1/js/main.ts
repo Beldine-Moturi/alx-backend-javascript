f6277c4a356867a0a73a1005fc36198d2549bd0b
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-05 02:03:25 +0300

    task 3: create class interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,7 +11,37 @@ interface Directors extends Teacher {
     numberOfReports: number;
 }
 
+// functions
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 let printTeacher: printTeacherFunction = (firstName, lastName): string => `${firstName[0]}.${lastName}`;
+
+// classes
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string):  StudentInterface;
+}
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+	displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor (firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+		return 'Currently working';
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
