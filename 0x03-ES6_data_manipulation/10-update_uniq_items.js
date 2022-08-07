b74073849e13721ef433af513ec82204326782ac
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-07 06:25:34 +0300

    fix errors

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 export default function updateUniqueItems(map) {
   if (map instanceof Map) {
-    for (const one of map.keys()) {
-      if (map.get(one) === 1) {
-        map.set(one, 100);
+    for (const key of map.keys()) {
+      if (map.get(key) === 1) {
+        map.set(key, 100);
       }
     }
     return map;
