1ce5e4d00ff8391fd8ed0874d80f5ac39dbea377
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-07 06:15:43 +0300

    fix errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,14 @@
 export default function cleanSet(set, startString) {
-  let arr = []
   let text = '';
+  const array = [];
+
   if (startString && typeof startString === 'string') {
-    const newArr = [...set].filter((element) => element.startsWith(startString));
-    newArr.forEach((element) => arr.push(element.slice(startString.length)));
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
+    }
+    text = array.join('-');
   }
-text = arr.join('-');
   return text;
 }
