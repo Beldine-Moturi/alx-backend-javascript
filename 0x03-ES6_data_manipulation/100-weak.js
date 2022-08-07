3139958ee63a6eb6d5997832da42570a80aaece7
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-07 06:38:28 +0300

    fix error

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,11 @@
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
+let count = 1;
 
-export default function queryAPI(endpoint) {
-  if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) >= 5) {
-      throw Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-    return;
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, 1);
 }
-
-module.exports = { queryAPI, weakMap };
