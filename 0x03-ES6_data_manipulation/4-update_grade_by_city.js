c200051fbb69bf4b233774e52b9b8c962dc19b67
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-07 04:44:36 +0300

    eslint compliance

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,9 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
   const newList = getListStudents.filter((obj) => obj.location === city).map((element) => {
     const grade = newGrades.find((obj) => obj.studentId === element.id);
-    element.grade = grade !== undefined ? grade.grade: 'N/A';
-    return element;
+    const newElement = { ...element };
+    newElement.grade = grade !== undefined ? grade.grade : 'N/A';
+    return newElement;
   });
   return newList;
-} 
+}
