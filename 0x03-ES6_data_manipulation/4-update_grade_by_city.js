1f3730f2c79dc819f962b20db3a469aa37f13715
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-07 04:29:51 +0300

    use array map function

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-  const newList = getListStudents.filter((obj) => obj.location === city);
-  newList.forEach(element => {
+  const newList = getListStudents.filter((obj) => obj.location === city).map((element) => {
     const grade = newGrades.find((obj) => obj.studentId === element.id);
     element.grade = grade !== undefined ? grade.grade: 'N/A';
+    return element;
   });
   return newList;
 } 
