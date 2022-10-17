1302e6e9709f64b3b0298739d5eaeca8c0e8ef12
Beldine-Moturi <beldinemoturi@gmail.com>
2022-10-17 13:20:51 +0300

    fix errors

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,111 +1,28 @@
-const chai = require('chai');
-const expect = chai.expect;
-const calculateNumber = require('./1-calcul');
+var chai  = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-describe("test rounding off numbers with SUM", function() {
-    describe("test two integers", function() {
-        it("tests two integers", function() {
-            expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-        });
-    });
-
-    describe("test rounding off numbers", function() {
-        it("tests sum int and float", function() {
-            expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
-        });
-    });
-    
-    describe("test rounding off numbers", function() {
-        it("tests sum two floats", function() {
-            expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-        });
-    });
-    
-    describe("test rounding off numbers", function() {
-        it("tests sum two floats", function() {
-            expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
-        });
-    });
-    
-    describe("calculates two floats rounded down:", function() {
-        it("tests sum of two floats", function() {
-            expect(calculateNumber('SUM', 1.2, 3.1)).to.equal(4);
-        });
-    });
-});
-
-describe("test rounding off numbers with SUBTRACT", function() {
-    describe("test subtraction of two integers", function() {
-        it("returns 2", function() {
-            expect(calculateNumber('SUBTRACT', 3, 1)).to.equal(2);
+describe('Test calculateNumber with chai', function() {
+    describe('Tests SUM:', function(){
+        it('should return 6', function() {
+            chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
         });
     });
 
-    describe("test subtraction of two integers", function() {
-        it("returns -2", function() {
-            expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
-        });
-    });
-    
-    describe("test subtraction of two integers", function() {
-        it("returns 2", function() {
-            expect(calculateNumber('SUBTRACT', 3.7, 1.2)).to.equal(3);
-        });
-    });
-    
-    describe("test subtraction of two integers", function() {
-        it("returns 2", function() {
-            expect(calculateNumber('SUBTRACT', 3.7, 1.5)).to.equal(2);
-        });
-    });
-    
-    describe("calculates two floats rounded down:", function() {
-        it("tests subtraction of two floats", function() {
-            expect(calculateNumber('SUBTRACT', 3.1, 1.2)).to.equal(2);
+    describe('Tests SUBTRACT:', function(){
+        it('should return -4', function() {
+            chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
         });
     });
 
-    describe("calculates two floats rounded down:", function() {
-        it("tests subtraction of two floats", function() {
-            expect(calculateNumber('SUBTRACT', -3.1, 1.2)).to.equal(-4);
-        });
-    });
-});
-
-describe("test rounding off numbers with DIVIDE", function() {
-    describe("test two integers", function() {
-        it("tests two integers", function() {
-            expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+    describe('Tests DIVIVDE:', function(){
+        it('should return 0.2', function() {
+            chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
         });
     });
 
-    describe("test rounding off numbers", function() {
-        it("tests divide int and float", function() {
-            expect(calculateNumber('DIVIDE', 3, 2)).to.equal(1.5);
-        });
-    });
-    
-    describe("test rounding off numbers", function() {
-        it("tests divide  two floats", function() {
-            expect(calculateNumber('DIVIDE', 3.7, 1.2)).to.equal(4);
-        });
-    });
-    
-    describe("test rounding off numbers", function() {
-        it("tests divide  two floats", function() {
-            expect(calculateNumber('DIVIDE', 3.7, 1.5)).to.equal(2);
-        });
-    });
-    
-    describe("calculates two floats rounded down:", function() {
-        it("tests divide  of two floats", function() {
-            expect(calculateNumber('DIVIDE', 3.1, 1.2)).to.equal(3);
-        });
-    });
-
-    describe("calculates two floats rounded down:", function() {
-        it("tests divide  of two floats", function() {
-            expect(calculateNumber('DIVIDE', 3.1, 0.2)).to.equal('Error');
+    describe('Tests DIVIVDE with 0:', function(){
+        it('should return Error', function() {
+            chai.expect(calculateNumber('DIVIDE', 8.3, 0)).to.equal('Error');
         });
     });
 });
